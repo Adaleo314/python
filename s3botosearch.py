3b8d3eda2a02c0b562c367cca9d37ffe2b0497ff
Adam Leonard <acleonard2@gmail.com>
2023-01-10 05:52:54 +0000

    updates

--- s3botosearch.py
+++ s3botosearch.py
@@ -1,12 +1,14 @@
 import boto3
 
-resource = boto3.resource('s3')
+resource = boto3.client('s3')
 
-#list # of buckets 
-bucket_list = list(resource.buckets.all())
-len(bucket_list)
-
+#list # of buckets
 #see all buckets in s3
 for bucket in resource.buckets.all():
     print(bucket.name)
 
+#listing creation date of buckets
+
+
+bucket in resource(['Buckets']):
+    print(bucket[Name])
