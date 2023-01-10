0242a7f89396f838da4cdf538a2bf11bec5bea4a
Adam Leonard <acleonard2@gmail.com>
2023-01-10 16:04:22 +0000

    updated code

--- s3policy.py
+++ s3policy.py
@@ -1,3 +1,11 @@
-"""
-Your module description
-"""
+import boto3
+
+s3_resource = boto3.client('s3')
+
+s3_resource.list_buckets()
+
+creation_date = s3_resource.list_buckets()["Buckets"][0]["CreationDate"]
+
+for bucket in s3_resource.list_buckets()['Buckets']:
+    print(bucket)
+    
