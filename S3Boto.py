683796bb169ece4344556ad14753630e8faf2fa3
Adam Leonard <acleonard2@gmail.com>
2023-01-10 04:16:25 +0000

    update

--- S3Boto.py
+++ S3Boto.py
@@ -1,7 +1,7 @@
 import boto3
 
 aws_resource = boto3.resource('s3')
-bucket = aws_resource.Bucket('adaleobucket')
+bucket = aws_resource.Bucket('adaleobucket314')
 
 response = bucket.create(
     ACL='public-read',
