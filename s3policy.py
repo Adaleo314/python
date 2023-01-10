3c38f91c09f7dc7096aa4f6fb42685b1d8681e83
Adam Leonard <acleonard2@gmail.com>
2023-01-10 16:07:25 +0000

    updated notes

--- s3policy.py
+++ s3policy.py
@@ -6,6 +6,7 @@ s3_resource.list_buckets()
 
 creation_date = s3_resource.list_buckets()["Buckets"][0]["CreationDate"]
 
+#use the created creation date variable to output creation date of specific buckets.
 for bucket in s3_resource.list_buckets()['Buckets']:
     print(bucket)
     
