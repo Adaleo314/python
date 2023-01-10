b4a2a05baf01c3ea617da5daf273509fdc9eb689
Adam Leonard <acleonard2@gmail.com>
2023-01-10 04:12:49 +0000

    boto update

---
+++ S3Boto.py
@@ -0,0 +1,13 @@
+import boto3
+
+aws_resource = boto3.resource('s3')
+bucket = aws_resource.bucket('adaleobucket')
+
+response = bucket.create(
+    ACL='public-read',
+    CreateBucketConfiguration={
+        'LocationConstraint': 'us-east-2'
+    },
+   
+)
+print(response)
