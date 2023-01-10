e8458fc02214fcb93e555fb816c7aa90be8c59c7
Adam Leonard <acleonard2@gmail.com>
2023-01-10 16:30:46 +0000

    updated code

--- s3uploads.py
+++ s3uploads.py
@@ -1,2 +1,15 @@
 import boto3
+import os
+import glob
+
+s3_resource = boto3.client('s3')
+
+#how to upload single file to s3
+    
+s3_resource.upload_file(
+    Filename='test.txt',
+    Bucket='mybucket-boto3',
+    Key='test.txt')
+    
+    
 
