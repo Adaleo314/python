399411faf8f2a48cf40d8a59f8ea343fbd21c099
Adam Leonard <acleonard2@gmail.com>
2023-01-10 17:58:46 +0000

    update

--- delete_s3bucket.py
+++ delete_s3bucket.py
@@ -2,15 +2,18 @@ import boto3
 
 s3_resource = boto3.client('s3')
 
-s3_resource.delete_object(Bucket='adaleo314bucket', Key='qomk9yntlnq41.webp')
+s3_resource.delete_object(Bucket='bucketname', Key='keyname')
 
 
 import os
 import glob
 
-s3_resource.list_objects(Bucket='adaleo314bucket')["Contents"]
+s3_resource.list_objects(Bucket='bucketname')["Contents"]
 
-
+#iteration
 for object in objects:
     print(object['Key'])
+    s3_resource.delete_object(Bucket='bucketname', Key=object['Key'])
     
+    
+        
