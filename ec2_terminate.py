63e6db1bf6287c1eca504439e13ae68b7d1526cd
Adam Leonard <acleonard2@gmail.com>
2023-01-11 19:20:43 +0000

    updated

--- ec2_terminate.py
+++ ec2_terminate.py
@@ -4,5 +4,14 @@ ec2_client=boto3.client('ec2')
 
 response=ec2_client.describe_instances()
 
-print(len['Reservations'])
-response.key()
+print(len(response['Reservations']))
+
+#############################
+
+data=response['Reservations']
+
+for instances in data:
+    instance=instances['Instances']
+    for ids in instance:
+        instance_id=ids['InstanceId']
+        print(instance_id)
