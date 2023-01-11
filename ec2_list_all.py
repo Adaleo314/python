02e55db017102a4f73edab363da3ab1fa83d025c
Adam Leonard <acleonard2@gmail.com>
2023-01-11 19:14:04 +0000

    updates

--- ec2_list_all.py
+++ ec2_list_all.py
@@ -6,4 +6,10 @@ ec2_client=boto3.client('ec2')
 
 response=ec2_client.describe_instances()
 
-print(response)
+#print(response)
+
+data=response['Reservations'][0] #Reservation is name of dictionary holding ec2 info
+data_instance=data['Instances']#Instances is the key
+for i in range (len(data_instance)):
+    print(f"instance id is {data_instance[i]['InstanceId']}")#instanceId is the value
+
