21f82f918616c4b14cc1bca305c44a547ad8e8f1
Adam Leonard <acleonard2@gmail.com>
2023-01-13 03:18:59 +0000

    update

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -3,7 +3,7 @@ import boto3
 #Create a list of input text
 
 def new_input_text_list():
-    input_text=open_input():
+    input_text=open_input()
     new_list=[]
     for item in input_text:
         text=item['item']
